1d32be2d461e752d0c9060c4f7f39596f5b0b567
Priyanshu kumar <priyanshukumar@Priyanshus-MacBook-Air.local>
2026-07-15 22:51:23 +0530

    fix error

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_URL;
+const API_BASE = process.env.REACT_APP_API_URL || "https://job-tracker-oujc.onrender.com/api";
 
 const api = axios.create({ baseURL: API_BASE });
 
